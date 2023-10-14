74334aff200adeb98c32ce0a035516006ca657a4
ccbezx <147884199+ccbezx@users.noreply.github.com>
2023-10-14 12:48:58 +0200

    Update FiltrowanieX.plugin.js

--- FiltrowanieX.plugin.js
+++ FiltrowanieX.plugin.js
@@ -1,49 +1,56 @@
 /**
-    * @name FiltrowanieX
-    * @source https://github.com/ccbezx/FiltrowanieX/blob/main/FiltrowanieX.plugin.js
-    * @description Plugin do filtrowania wiadomości w Discord.
-    * @updateUrl https://github.com/ccbezx/FiltrowanieX/raw/main/FiltrowanieX.plugin.js
-    * @website https://github.com/ccbezx/FiltrowanieX
-    * @version 0.0.1
-    */
+ * @name FiltrowanieX
+ * @source https://github.com/ccbezx/FiltrowanieX/blob/main/FiltrowanieX.plugin.js
+ * @description Plugin do filtrowania wiadomości w Discord.
+ * @updateUrl https://github.com/ccbezx/FiltrowanieX/raw/main/FiltrowanieX.plugin.js
+ * @website https://github.com/ccbezx/FiltrowanieX
+ * @version 0.0.1
+ */
 
-(function() {
-    'use strict';
+const FiltrowanieX = class {
+    constructor() {
+        this.blockedUserIds = ['1162699952280064040', '440220579849699329'];
+        this.blockedKeywords = ['penis', 'sex'];
+    }
 
-    // Zablokowane ID użytkowników
-    const blockedUserIds = ['379633830572851210', '440220579849699329'];
-
-    // Zablokowane słowa kluczowe
-    const blockedKeywords = ['penis', 'sex'];
-
-    // Funkcja do filtrowania wiadomości
-    const messageFilter = () => {
-        const messages = document.getElementsByClassName('message-content');
+    start() {
+        const messages = document.querySelectorAll('.containerCozyBounded-ujftM0');
 
         for (const message of messages) {
             const messageText = message.textContent;
 
             // Sprawdź, czy wiadomość zawiera zablokowane ID użytkowników
-            for (const userId of blockedUserIds) {
+            for (const userId of this.blockedUserIds) {
                 if (messageText.includes(userId)) {
-                    message.textContent = 'Zablokowana wiadomość';
+                    this.hideMessage(message);
                     return;
                 }
             }
 
             // Sprawdź, czy wiadomość zawiera zablokowane słowa kluczowe
-            for (const keyword of blockedKeywords) {
+            for (const keyword of this.blockedKeywords) {
                 if (messageText.includes(keyword)) {
-                    message.textContent = 'Zablokowana wiadomość';
+                    this.hideMessage(message);
                     return;
                 }
             }
 
             // Usuń linki z wiadomości
-            message.innerHTML = messageText.replace(/(https?|ftp):\/\/[^\s/$.?#].[^\s]*/gi, '');
+            message.innerHTML = this.removeLinks(messageText);
         }
-    };
+    }
 
-    // Wywołaj funkcję messageFilter przy każdym zdarzeniu DOMContentLoaded
-    document.addEventListener('DOMContentLoaded', messageFilter);
-})();
+    stop() {
+        // Zatrzymaj filtrowanie
+    }
+
+    hideMessage(message) {
+        message.innerHTML = '<span style="color: red;">Zablokowana wiadomość</span>';
+    }
+
+    removeLinks(messageText) {
+        return messageText.replace(/(https?|ftp):\/\/[^\s/$.?#].[^\s]*/gi, '');
+    }
+};
+
+module.exports = FiltrowanieX;
